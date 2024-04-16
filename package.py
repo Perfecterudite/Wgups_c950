8227266b0fe992754b3bb1bb64fb29b03c588da3
Perfecterudite <ayomideadedeji99@yahoo.com>
2024-04-15 22:44:44 -0500

    created csv files

--- package.py
+++ package.py
@@ -1,1 +1,19 @@
+#This is a comment
+
 print(f'hello')
+if 5 > 2:
+    print("five is greater than two")
+""" hey 
+this is also a comment
+"""
+
+x = 'you'
+
+def myfunc():
+   global x #larger scope
+   x = 'u'
+   print("this is " + x)
+
+myfunc()
+
+print("this is " + x)
